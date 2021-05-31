51eea5a8f138c133b9c15f74b08ee31b9a7a8542
Axel Björnfot <axelbjornfot@gmail.com>
2021-05-31 10:53:30 +0200

    UserProvider and added a css folder

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,11 @@
-const Home = () => {};
+import { useContext, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { UserContext } from "../context/UserProvider";
+
+import styles from "./css/Home.module.css";
+
+const Home = () => {
+  return <div className='Home'></div>;
+};
 
 export default Home;
